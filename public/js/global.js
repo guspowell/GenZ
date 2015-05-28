a34e71cfcfc0cdb9597c1def59c744e8904a8c13
guspowell <augustuspowell@me.com>
2015-05-28 14:16:41 +0100

    weird margin on right side

---
+++ public/js/global.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//
+//   $('#image-container #content').each(function() {
+//       $(this).css('margin-top', $(this).parent().height()-$(this).height())
+//   });
+//
+// });
