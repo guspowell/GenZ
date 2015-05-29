2d7cf0d4c9c4c003a9c082a86608f9d05f0e96d4
guspowell <augustuspowell@me.com>
2015-05-29 17:20:50 +0100

    scrolling not working on iphone. testing

--- public/js/global.js
+++ public/js/global.js
@@ -6,10 +6,21 @@ $(document).ready(function(){
 
   if ($(window).width() > 768) {
     $('#image-container.card6 #content').css('margin-top', 0);
-  }
+  };
 
   if ($(window).width() < 768) {
     $('#image-container.card9').css('color', 'white');
-  }
+  };
+
+  setTitleContainerHeight();
+
+  skrollr.init();
 
 });
+
+function setTitleContainerHeight() {
+  var windowHeight = window.innerHeight;
+  var navbarHeight = 50;
+  var height = "height:" + (windowHeight - navbarHeight - 88) + "px;";
+  document.getElementById('title-container').setAttribute('style', height)
+};
