85ab6ff7b8852ae65c12481c609cd58849261aba
guspowell <augustuspowell@me.com>
2015-06-05 12:29:45 +0100

    merged globaljs

--- public/js/global.js
+++ public/js/global.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
   positionCardText();
 
-  if ($(window).width() > 768) {
-    $('#image-container.card6 #content').css('margin-top', 0);
-  };
+  // if ($(window).width() > 768) {
+  //   $('#image-container.card6 #content').css('margin-top', 0);
+  // };
 
   if ($(window).width() < 768) {
     $('#image-container.card9').css('color', 'white');
@@ -26,17 +26,18 @@ $(document).ready(function(){
 function setTitleContainerHeight() {
   var windowHeight = window.innerHeight;
   var navbarHeight = 50;
-  var paddingHeight = parseInt( $("#yellow-container").css('padding-top').replace("px", "") );
+  var paddingHeight = parseInt( $("#yellow-section-1").css('padding-top').replace("px", "") );
   var buttonHeight = $("btn.read-report").outerHeight();
   var setHeight = windowHeight - navbarHeight - paddingHeight*2 - buttonHeight;
   $('#title-container').css("height", setHeight);
 };
 
 function positionCardText() {
-  $('#image-container #content').each(function() {
-      $(this).css('margin-top', $(this).parent().height()-$(this).height());
+  $('.image-container .content').each(function() {
+      console.log($(this).parent().height());
+      $(this).css('margin-top', $(this).parents('.image-container').height()-$(this).height());
   });
-  $('#image-container #contentPNG').each(function() {
-      $(this).css('margin-top', ( $(this).parent().height()/2 ) - ( $(this).height()/2 ));
+  $('.image-container .contentPNG').each(function() {
+      $(this).css('margin-top', ( $(this).parents('.image-container').height()/2 ) - ( $(this).height()/2 ));
   });
 };
