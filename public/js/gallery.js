7ec58fcbe8643880b4a62704583b696142c3ad63
guspowell <augustuspowell@me.com>
2015-06-08 16:07:44 +0100

    all images in an array and working in slideshow

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -19,31 +19,69 @@ function calcWindowWidth() {
   return $(window).width();
 };
 
-// function loadCarouselImages(selector) {
-//   var imagesJson = {
-//     "generation-entrepreneur" : {
-//       "@1x" : ["10-marketing-with-etiquette.jpg","02_shifting_gender_norms.png", "04-embracing-extremes.jpg"],
-//       "@2x" : ["10-marketing-with-etiquette.jpg","02_shifting_gender_norms.png", "04-embracing-extremes.jpg"],
-//     },
-//     "shifting-gender-norms" : {
-//       "@1x" : ["08-privacy-tightrope.jpg", "09-creative-communicators.png"],
-//       "@2x" : ["08-privacy-tightrope.jpg", "09-creative-communicators.png"]
-//   }
-//
-//   var imageArray = imagesJson[selector]
-//
-//   var content = "";
-//
-//   for(var i=0; i<imageArray.length; i++) {
-//     var activeImage = imageArray[0]
-//     var image = imageArray[i];
-//     content += "<div class='item'>" +
-//               "<img src='images/" + image + "' alt='Chania' width='460' height='345'>" +
-//               "</div>";
-//     $(".carousel-inner").html(content);
-//     $(".carousel-inner .item").first().attr('class', 'item active');
-//   };
-// };
+function loadCarouselImages(selector) {
+  console.log(selector)
+  var imagesJson = {
+    "01-ellie" : {
+      "@1x" : ["01@1x.jpg","02@1x.jpg", "03@1x.jpg", "04@1x.jpg", "05@1x.jpg"],
+      "@2x" : ["01@2x.jpg","02@2x.jpg", "03@2x.jpg", "04@2x.jpg", "05@2x.jpg"],
+    },
+    "02-bella-and-jo" : {
+      "@1x" : ["02-bellajo-01@1x.jpg", "02-bellajo-02@1x.jpg", "02-bellajo-03@1x.jpg", "02-bellajo-04@1x.jpg", "02-bellajo-05@1x.jpg"],
+      "@2x" : ["02-bellajo-01@2x.jpg", "02-bellajo-02@2x.jpg", "02-bellajo-03@2x.jpg", "02-bellajo-04@2x.jpg", "02-bellajo-05@2x.jpg"],
+    },
+    "03-eligh" : {
+      "@1x" : ["03-eligh-01@1x.jpg", "03-eligh-02@1x.jpg", "03-eligh-03@1x.jpg", "03-eligh-04@1x.jpg"],
+      "@2x" : ["03-eligh-01@2x.jpg", "03-eligh-02@2x.jpg", "03-eligh-03@2x.jpg", "03-eligh-04@2x.jpg"],
+    },
+    "04-elliot" : {
+      "@1x" : ["04-elliot-01@1x.jpg", "04-elliot-02@1x.jpg", "04-elliot-03@1x.jpg", "04-elliot-04@1x.jpg", "04-elliot-05@1x.jpg"],
+      "@2x" : ["04-elliot-01@2x.jpg", "04-elliot-02@2x.jpg", "04-elliot-03@2x.jpg", "04-elliot-04@2x.jpg", "04-elliot-05@2x.jpg"],
+    },
+    "05-alessandra" : {
+      "@1x" : ["05-alessandra-01@1x.jpg", "05-alessandra-02@1x.jpg", "05-alessandra-03@1x.jpg", "05-alessandra-04@1x.jpg", "05-alessandra-05@1x.jpg"],
+      "@2x" : ["05-alessandra-01@2x.jpg", "05-alessandra-02@2x.jpg", "05-alessandra-03@2x.jpg", "05-alessandra-04@2x.jpg", "05-alessandra-05@2x.jpg"],
+    },
+    "06-lewis" : {
+      "@1x" : ["06-lewis-01@1x.jpg", "06-lewis-02@1x.jpg", "06-lewis-03@1x.jpg", "06-lewis-04@1x.jpg", "06-lewis-05@1x.jpg"],
+      "@2x" : ["06-lewis-01@2x.jpg", "06-lewis-02@2x.jpg", "06-lewis-03@2x.jpg", "06-lewis-04@2x.jpg", "06-lewis-05@2x.jpg"],
+    },
+    "07-simran" : {
+      "@1x" : ["07-simran-01@1x.jpg", "07-simran-02@1x.jpg", "07-simran-03@1x.jpg", "07-simran-04@1x.jpg", "07-simran-05@1x.jpg"],
+      "@2x" : ["07-simran-01@2x.jpg", "07-simran-02@2x.jpg", "07-simran-03@2x.jpg", "07-simran-04@2x.jpg", "07-simran-05@2x.jpg"],
+    },
+    "08-rose" : {
+      "@1x" : ["08-rose-01@1x.jpg", "08-rose-02@1x.jpg", "08-rose-03@1x.jpg", "08-rose-04@1x.jpg", "08-rose-05@1x.jpg"],
+      "@2x" : ["08-rose-01@2x.jpg", "08-rose-02@2x.jpg", "08-rose-03@2x.jpg", "08-rose-04@2x.jpg", "08-rose-05@2x.jpg"],
+    },
+    "09-henry" : {
+      "@1x" : ["09-henry-01@1x.jpg", "09-henry-02@1x.jpg", "09-henry-03@1x.jpg", "09-henry-04@1x.jpg", "09-henry-05@1x.jpg"],
+      "@2x" : ["09-henry-01@2x.jpg", "09-henry-02@2x.jpg", "09-henry-03@2x.jpg", "09-henry-04@2x.jpg", "09-henry-05@2x.jpg"],
+    },
+    "10-jaysea" : {
+      "@1x" : ["10-jaysea-01@1x.jpg", "10-jaysea-02@1x.jpg", "10-jaysea-03@1x.jpg"],
+      "@2x" : ["10-jaysea-01@2x.jpg", "10-jaysea-02@2x.jpg", "10-jaysea-03@2x.jpg"],
+    },
+  };
+
+
+  SmallImageArray = imagesJson[selector]["@1x"];
+  LargeImageArray = imagesJson[selector]["@2x"];
+  var content = "";
+
+  for(var i=0; i<SmallImageArray.length; i++) {
+    var onexSrc = "images/bios/" + selector + "/interests/@1x/" + imagesJson[selector]["@1x"][i] + " 1x";
+    var twoxSrc = "images/bios/" + selector + "/interests/@2x/" + imagesJson[selector]["@1x"][i] + " 2x";
+
+    content +=  "<div class='item'>" +
+                "<img srcset=" + '\"' + onexSrc + ", " + twoxSrc + '\"' + " alt='Chania' width='460' height='345'/>" +
+                "</div>";
+
+    $(".carousel-inner").html(content);
+    $(".carousel-inner .item").first().attr('class', 'item active');
+  };
+
+};
 
 function carouselHeight() {
   var height = $("#image-gallery").height();
@@ -61,11 +99,10 @@ function setArrowPosition() {
 function fadeOutImages() {
   $(".grid-item").click(function() {
     if (calcWindowWidth() >= 650) {
-      // loadCarouselImages($(this).attr('rel'));
+      loadCarouselImages($(this).attr('rel'));
       $(".grid").css({background:"transparent"});
       $(".grid").css("pointer-events","none");
       var selected = this;
-      console.log($(selected).children(".image-rollover"));
       $(selected).children(".image-rollover").css("opacity", "1");
       $(function() {
         $('.grid div').sort(function(){
